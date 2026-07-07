7faa920484de09232bab9d572f1e53368dc0904e
bugaev <bugaev1404g@mail.ru>
2026-07-08 01:17:29 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -23,7 +23,7 @@ def pytest_addoption(parser):
         "--browser_version",
         default="128.0",
         help="Browser version to use. Versions 124.0 and 125.0 for Firefox, other for Chrome",
-        choices=("127.0", "128.0", "144.0", "145.0" "149.0", "150.0")
+        choices=("127.0", "128.0", "144.0", "145.0", "149.0", "150.0")
     )
     parser.addoption(
         "--headless",
