14835753a96a80af48e8a950d89967630157ae5f
bugaev <bugaev1404g@mail.ru>
2026-07-08 14:25:57 +0300

    Вывел размер экрана в консоль

--- conftest.py
+++ conftest.py
@@ -88,6 +88,8 @@ def setup_browser(request):
         command_executor=command_executor,
         options=options
     )
+    window = driver.get_window_size()
+    print(window)
 
     yield driver
 
